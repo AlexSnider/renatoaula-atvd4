9f4614c5a3e331131dcf9725292838aba85bf2b0
AlexSnider <alexsnider8@gmail.com>
2024-06-10 10:29:44 -0300

    refactor: add new get routes and changes mysql host connection

--- index.js
+++ index.js
@@ -4,10 +4,22 @@ const app = express();
 const port = 3000;
 
 const db = mysql2.createConnection({
-  host: "localhost",
+  host: "172.17.0.2",
   user: "user",
   password: "passwd",
   database: "db_aula",
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/liveness", (req, res) => {
+  res.status(200).send("OK");
+});
+
+app.get("/readiness", (req, res) => {
+  res.status(200).send("Ready to handle requests");
 });
 
 app.get("/consulta-dados", (req, res) => {
@@ -17,9 +29,7 @@ app.get("/consulta-dados", (req, res) => {
       res.status(500).send("Erro na consulta...");
       return;
     }
-    res
-      .status(200)
-      .json({ message: "Dados encontrados!", data: results });
+    res.status(200).json({ message: "Dados encontrados!", data: results });
   });
 });
 
